24205c7976dbd44f9f700aa5437556196ab211e4
masingerzero <masingerzero@gmail.com>
2017-12-03 22:39:03 +0100

    first commit

---
+++ spring-boot-tests/src/main/java/org/masingerzero/springboottests/SpringBootTestsApplication.java
@@ -0,0 +1,12 @@
+package org.masingerzero.springboottests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestsApplication.class, args);
+	}
+}
